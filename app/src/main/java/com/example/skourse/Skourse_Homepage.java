fd931f58985a0077a77b66dcc50e83f3c54abdac
daurelius <55678808+daurelius@users.noreply.github.com>
2019-11-26 01:35:12 +0700

    HomePage : search + card_course
    just xml design

---
+++ app/src/main/java/com/example/skourse/Skourse_Homepage.java
@@ -0,0 +1,14 @@
+package com.example.skourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Skourse_Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skourse__homepage);
+    }
+}
