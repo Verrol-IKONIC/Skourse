361d30d704060c709469f023275fe53f67028081
Reinaldo <rlordianto@student.ciputra.ac.id>
2019-11-27 15:06:02 +0700

    menu Search

--- app/src/main/java/com/example/skourse/Skourse_Search.java
+++ app/src/main/java/com/example/skourse/Skourse_Search.java
@@ -1,14 +1,39 @@
 package com.example.skourse;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Skourse_Search extends AppCompatActivity {
-
+    ActionBar bar;
+    ImageButton back;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skourse_search);
+        bar =getSupportActionBar();
+        bar.hide();
+
+        back = findViewById(R.id.btn_back);
+        back.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            backtoMain();
+        }
+    });
+
     }
+
+    public void backtoMain(){
+        Intent intent = new Intent(this, Skourse_Main.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/skourse/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/skourse/ui/home/HomeFragment.java
@@ -1,12 +1,15 @@
 package com.example.skourse.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.skourse.R;
+import com.example.skourse.Skourse_Search;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,7 +18,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 public class HomeFragment extends Fragment {
-
+    Button btn_searchs;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,6 +26,13 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        btn_searchs = (Button)root.findViewById(R.id.btn_search);
+        btn_searchs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                test();
+            }
+        });
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -31,5 +41,10 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    public void test(){
+        Intent intent = new Intent(getActivity(), Skourse_Search.class);
+        startActivity(intent);
     }
 }
