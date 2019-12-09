5a57541a8a81fa6d66791632a26f04236830f3e3
daurelius <55678808+daurelius@users.noreply.github.com>
2019-12-09 10:19:34 +0700

    Ngubah Activity homepage jadi HompageFragment
    layar homepage masih double

--- app/src/main/java/com/example/skourse/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/skourse/ui/home/HomeFragment.java
@@ -10,12 +10,16 @@ import android.widget.TextView;
 
 import com.example.skourse.R;
 import com.example.skourse.Skourse_Search;
+import com.example.skourse.adapters.HorizontalCategoryAdapter;
+import com.example.skourse.adapters.HorizontalCourseAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends Fragment {
     Button btn_searchs;
@@ -23,24 +27,29 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        btn_searchs = (Button)root.findViewById(R.id.btn_search);
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btn_searchs = view.findViewById(R.id.button_search);
         btn_searchs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 test();
             }
         });
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        RecyclerView list_course = view.findViewById(R.id.RecyclerView_course);
+        list_course.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        list_course.setAdapter(new HorizontalCourseAdapter(new String[]{"Alpro Course", "MobileApp Course", "Vocal Course"}));
+
+        RecyclerView list_recently = view.findViewById(R.id.RecyclerView_recentlyBooked);
+        list_recently.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        list_recently.setAdapter(new HorizontalCourseAdapter(new String[]{"MobileApp Course", "Vocal Course"}));
+
+        RecyclerView list_category = view.findViewById(R.id.RecyclerView_category);
+        list_category.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        list_category.setAdapter(new HorizontalCategoryAdapter(new String[]{"programming", "music", "art", "social", "programming", "music", "art", "social"}));
+
+        return view;
     }
 
     public void test(){
